d88f30c3c26bea5dadf5ce4db76778b15dfa6213
goldsami <zolotarenkomax@gmail.com>
2022-03-01 22:50:09 +0200

    feat: game end added

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const App = {
       gameSpeed: 500,
       snake: {
         direction: null,
-        cells: [
-          {x: 5, y: 5}
-        ]
+        cells: []
       },
+      food: null,
       timeoutId: null
     };
   },
   methods: {
     start() {
+      this.snake.cells = [
+          {x: 3, y: 3}, {x: 3, y: 4}, {x: 3, y: 5}, {x: 4, y: 5}, {x: 5, y: 5}, {x: 5, y: 6}, {x: 5, y: 7}
+      ]
       if (this.timeoutId) clearInterval((this.timeoutId))
       this.timeoutId = setInterval(() => {
         console.log('interval')
@@ -25,36 +27,41 @@ const App = {
       return !!this.snake.cells.find(c => c.x === x && c.y === y)
     },
     makeStep() {
+      let newCell;
       switch (this.snake.direction) {
         case 'up':
-          this.snake.cells.unshift({
+          newCell = {
             x: this.returnInRange(this.snake.cells[0].x - 1),
             y: this.returnInRange(this.snake.cells[0].y)
-          })
+          }
           this.snake.cells.pop()
           break
         case 'left':
-          this.snake.cells.unshift({
+          newCell = {
             x: this.returnInRange(this.snake.cells[0].x),
             y: this.returnInRange(this.snake.cells[0].y - 1)
-          })
+          }
           this.snake.cells.pop()
           break
         case 'down':
-          this.snake.cells.unshift({
+          newCell = {
             x: this.returnInRange(this.snake.cells[0].x + 1),
             y: this.returnInRange(this.snake.cells[0].y)
-          })
+          }
           this.snake.cells.pop()
           break
         case 'right':
-          this.snake.cells.unshift({
+          newCell = {
             x: this.returnInRange(this.snake.cells[0].x),
             y: this.returnInRange(this.snake.cells[0].y + 1)
-          })
+          }
           this.snake.cells.pop()
           break
       }
+      if (this.isSnake(newCell.x, newCell.y)) {
+        this.endGame()
+      }
+      this.snake.cells.unshift(newCell);
     },
     spacePressHandler(e) {
       if (e.code === 'Space') {
@@ -81,6 +88,14 @@ const App = {
       if (value > end) return value - end
       else if (value < start) return  value + end
       else  return  value
+    },
+    endGame() {
+      alert('Mortis')
+      clearInterval(this.timeoutId)
+      this.snake.direction = null
+    },
+    generateFood() {
+
     }
   },
   created() {
